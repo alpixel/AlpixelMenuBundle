0f3ee767979a5bc3d8697cd957e483a60f2a2514
Benjamin HUBERT <benjamin@alpixel.fr>
2016-02-19 17:46:03 +0100

    adding datafixtures

---
+++ DataFixtures/ORM/LoadMenuData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Alpixel\Bundle\MenuBundle\DataFixtures\ORM;
+
+use Alpixel\Bundle\MenuBundle\Entity\Menu;
+use Alpixel\Bundle\MenuBundle\Entity\MenuItem;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadMenuData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
+{
+    function load(ObjectManager $objectManager)
+    {
+	$menu = new Menu;
+	$menu->setMachineName('main');
+	$menu->setName('Menu principal');
+
+	$item = new MenuItem;
+	$item->setUri('/');
+	$item->setName('Homepage');
+	$menu->addItem($item);
+
+        $manager->persist($item);
+	$manager->persist($menu);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 0;
+    }
+}
