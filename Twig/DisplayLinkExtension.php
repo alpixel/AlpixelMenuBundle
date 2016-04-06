8189df9c5e745d1310407c0b3e01db077521f39f
Alexis Bussieres <alexis@alpixel.fr>
2016-04-06 11:33:33 +0200

    Add Twig extension to display link in menu and add the path app_dev if need

---
+++ Twig/DisplayLinkExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Alpixel\Bundle\MenuBundle\Twig;
+
+class DisplayLinkExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('alpixel_menu_link', [$this, 'menuLink'], ['needs_environment' => true]),
+        ];
+    }
+
+    /**
+     * Add app_dev.php in dev environment if the link is locale
+     *
+     * @param \Twig_Environment $twig
+     * @param $link
+     * @return string
+     */
+    public function menuLink(\Twig_Environment $twig, $link)
+    {
+        $globals = $twig->getGlobals();
+        $app = $globals['app'];
+        $env = $app->getEnvironment();
+
+        if ($env === 'dev' && strpos($link, 'http') === false) {
+            $dev = ($link[0] === '/') ? 'app_dev.php' : 'app_dev.php/';
+            $link = $dev.$link;
+        }
+
+        return $link;
+    }
+
+    public function getName()
+    {
+        return 'alpixel_menu_menu_link';
+    }
+}
